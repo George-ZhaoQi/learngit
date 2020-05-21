828d38d02c0873deb57750769d727154485a012d
ZhaoQi <zhao2005hit@163.com>
2020-05-21 14:51:48 +0800

    取消默认的注释行

--- test/src/test/test.java
+++ test/src/test/test.java
@@ -3,7 +3,7 @@ package test;
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		int j = 2;
 		for(int i=0; i<j ; i++) {
 			System.out.println("a!");
